03398c89626a54a531e2e438622f253b88a2d7e5
PRAGADHEESHWARI-2006 <pragadheeshwarisivakumar@gmail.com>
2026-07-08 12:09:19 +0100

    Update SecurityConfig.java

--- src/main/java/com/example/PeakPerform/config/SecurityConfig.java
+++ src/main/java/com/example/PeakPerform/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
                                 "http://localhost:5501",
                                 "http://127.0.0.1:5501",
                                 "http://localhost:5500",
-                                "http://127.0.0.1:5500","https://pragadheeshwari-2006.github.io/PeakPerformFrontend/"));
+                                "http://127.0.0.1:5500","https://pragadheeshwari-2006.github.io"));
 
                 configuration.setAllowedMethods(java.util.List.of(
                                 "GET",
